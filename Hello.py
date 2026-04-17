bf746e9253ce81421bcd8af8b516ac1051a79b20
snishtha20 <snishtha2006@gmail.com>
2026-04-17 22:33:35 +0530

    Learned about sep & end

--- Hello.py
+++ Hello.py
@@ -1,1 +1,14 @@
 print("Hello World!")
+
+print("India", "Pakistan", "Nepal", "China")
+
+# sep
+print("India", "Pakistan", "Nepal", "China", sep = '/')
+print("India", "Pakistan", "Nepal", "China", sep = '-')
+
+# end
+print("Hello")
+print("World")
+
+print("Hello", end=' ')
+print("World")
