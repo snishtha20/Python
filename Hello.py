64d1b2a7aeee730221dde5b91cf8c603e9650383
snishtha20 <snishtha2006@gmail.com>
2026-05-07 13:14:41 +0530

    Additional Knowledge

--- Hello.py
+++ Hello.py
@@ -10,5 +10,5 @@ print("India", "Pakistan", "Nepal", "China", sep = '-')
 print("Hello")
 print("World")
 
-print("Hello", end=' ')
+print("Hello", end=' ') #dono sentence ko ek hi line me combine kr deta h
 print("World")
