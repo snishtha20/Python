4f45f7b6904728ffe67b73362551429c203c6670
snishtha20 <snishtha2006@gmail.com>
2026-05-07 13:10:50 +0530

    For Loop codes

---
+++ Loops/For/for1.py
@@ -0,0 +1,28 @@
+# practice
+
+# list = [1,4,9,16,25,36,49,64,81,100]
+
+# for el in list:
+#     print(el)
+
+# 2nd
+# list = [1,4,9,16,25,36,49,64,81,100]
+# x = int(input("Enter a number: "))
+# idx = 0
+# for val in list:
+#     if(val == x):
+#         print("x found at index ", idx)
+#         break
+#     idx += 1
+# else:
+#     print("No found")
+
+
+# 3rd factorial of first n numbers
+
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact *= i
+#     i += 1
+# print(fact)
