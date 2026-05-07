4383610acf8c7deaef66e5504a1a622833b9b04f
snishtha20 <snishtha2006@gmail.com>
2026-05-07 13:10:15 +0530

    While Loop codes

---
+++ Loops/While/WhilewithElse.py
@@ -0,0 +1,6 @@
+x =1
+while x<3:
+    print(x)
+    x+=1
+else:
+    print("limit crossed")
---
+++ Loops/While/nums.py
@@ -0,0 +1,52 @@
+# Practice
+# i = 1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+
+# j =100
+# while j>=1:
+#     print(j)
+#     j-=1
+
+
+
+# 2nd
+# n = int(input("Enter a number: "))
+# i = 1
+# while i<=10:
+#     print(n*i)
+#     i+=1
+
+# 3rd
+# i =1
+# while i<=10:
+#     print(i*i)
+#     i+=1
+
+#4th
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# i = 0
+# while i < len(nums):
+#     print(nums[i])
+#     i+=1
+
+# 5th
+# nums = [1,4,9,16,25,36,49,64,81,100]
+
+# x = int(input("Enter a number: "))
+# i = 0
+# while i<len(nums):
+#     if(nums[i] == x):
+#         print("Found at index: ",i)
+#     i+=1
+
+# 6th Sum of first n numbers
+n = int(input("Enter a number: "))
+i = 1
+sum = 0
+while i<=n:
+    sum += i
+    i+=1
+print(sum)
