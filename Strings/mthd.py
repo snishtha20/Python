4e8aa49474c3b08e781d09e2fef69c28d2733cdd
snishtha20 <snishtha2006@gmail.com>
2026-05-08 00:32:15 +0530

    String Methods

--- Strings/mthd.py
+++ Strings/mthd.py
@@ -1,3 +1,91 @@
+# Creating Strings
+s = 'hello'
+s = "hello"
+#for multiple lines
+s = '''hello''' 
+s = """hello"""
+s =str('hello')
+print(s)
+
+
+# Accessing Strings
+# Positive Indexing
+s = 'hello world'
+print(s[1])
+# Negative Indexing: it's use when u don't know the length of string still u can extract last element of string
+s = 'hello world'
+print(s[-1])
+
+
+# Slicing
+s = 'hello world'
+print(s[0,5])
+print(s[::-1])
+print(s[-1:-6:-1])
+
+
+# Adding Chars to Strings 
+# Editing Strings: Python Strings are immutable
+# Deleting Strings
+s = 'hello world'
+del s
+print(s) #error: name 's' is not found
+s = 'hello world'
+del s[-1:-5:2]
+print(s) #throw error because in python strings are immutable so can't modify in any way
+
+
+# Operations on Strings
+# Arithmetic Operations
+print('delhi' + 'mumbai') #delhimumbai
+print('delhi' * 5) #delhidelhidelhidelhidelhi
+
+# Relational Operations
+'delhi' == 'delhi' #true
+'delhi' != 'delhi' #false
+'mumbai' > 'pune' #false because it checks lexiographically means according to ASCII- Jaise ABCD me P baad me aata h to wo bada maana jata h
+'Pune' > 'pune' #Capital letters ki ASCII Value kam hoti h small se
+
+# Logical Operations
+# In python, empty string considered as False
+" " or 'world' #world
+" " and 'world' #" "
+
+'hello' and 'world'  #world: because for and, both must true so python has to check 2nd string also to get true so it gives 2nd string in result
+'hello' or 'world' #hello: because for or, only one must true so as it gets true in 1st string it don't traverse forward and gives 1st string in reuslt
+
+not " " #True
+not "hello" #False
+
+# Loops on Strings
+for i in 'hello':
+    print(i)
+
+for i in 'delhi':
+    print('pune')
+
+
+# Membership Operations
+'D' in 'Delhi' #True
+'D' not in 'Delhi' #False
+'D' in 'delhi' #False
+
+
+# String Functions
+len("hello world") #11 include space
+max("hello world") #w according to ASCII Values
+min("hello world") #' ' space has lowest ASCII Value
+sorted("hello world") 
+sorted("hello world", reverse=True) #['w', 'r', 'o', 'o', 'l','l','l','h','e','d',' ']
+
+
+
+
+
+
+
+
+
 str = "i am a coder"
 
 print(str.endswith("er"))
@@ -21,7 +109,7 @@ print(str.count("a"))
 # | ------------------------ | ------------------------------------------------------ | ----------------------------------- |
 # | `str.find(sub)`          | Returns lowest index of substring or `-1` if not found | `"hello".find("l")` → `2`           |
 # | `str.rfind(sub)`         | Returns highest index of substring or `-1`             | `"hello".rfind("l")` → `3`          |
-# | `str.index(sub)`         | Like `find()`, but raises `ValueError` if not found    | `"hello".index("e")` → `1`          |
+# | `str.index(sub)`         | Like `find()`, but raises `ValueError` if not found    | `"hello".index("e")` → `1`          | #index and find is same only difference is find generates -1 in case no element is found but index generate error
 # | `str.rindex(sub)`        | Like `rfind()`, but raises `ValueError` if not found   | `"hello".rindex("l")` → `3`         |
 # | `str.startswith(prefix)` | Checks if string starts with prefix                    | `"hello".startswith("he")` → `True` |
 # | `str.endswith(suffix)`   | Checks if string ends with suffix                      | `"hello".endswith("lo")` → `True`   |
@@ -59,3 +147,19 @@ print(str.count("a"))
 # | `str.format(*args, **kwargs)`    | Formats string using placeholders                   | `"Hello {}".format("Alice")` → `'Hello Alice'`                          |
 # | `str.removeprefix(prefix)`       | Removes prefix if present (Python 3.9+)             | `"HelloWorld".removeprefix("Hello")` → `'World'`                        |
 # | `str.removesuffix(suffix)`       | Removes suffix if present (Python 3.9+)             | `"filename.txt".removesuffix(".txt")` → `'filename'`                    |
+
+
+# More functions
+# 1. split
+'hi my name is Nishtha'.split() #['hi', 'my', 'name', 'is', 'Nishtha']
+'hi my name is Nishtha'.split('i') #['h', 'my name', 's N', 'shtha']
+
+# 2. join
+" ".join(['hi', 'my', 'name', 'is', 'Nishtha']) #'hi my name is Nishtha'
+"-".join(['hi', 'my', 'name', 'is', 'Nishtha']) #'hi-my-name-is-Nishtha'
+
+# 3. replace
+'hi my name is Nishtha'.replace('Nishtha', 'Mishthi') #'hi my name is Mishthi'
+
+
+# Every function genrating new string with required changes. No changes happens in original string
