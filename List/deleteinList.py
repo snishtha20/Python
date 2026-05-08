d2dee2401ffabfaca707f34dda4dfa338fdca2f4
snishtha20 <snishtha2006@gmail.com>
2026-05-08 20:03:03 +0530

    Deleting items in List

---
+++ List/deleteinList.py
@@ -0,0 +1,33 @@
+# del: index btana padta
+# L=[1,2,3,4,5]
+# print(L)
+# del L
+# print(L)
+
+# M=[1,2,3,4,5]
+# indexing
+# del M[-1]
+# print(M)
+
+# slicing
+# del M[1:3]
+# print(M)
+
+# remove: without index direct value dekr ho jata h
+# L=[1,2,3,4,5]
+# L.remove(5)
+# print(L)
+
+
+# pop: default remove last element
+# L=[1,2,3,4,5]
+# L.pop(0)
+# print(L)
+
+# L.pop()
+# print(L)
+
+# clear: all elments delete, give empty list
+L=[1,2,3,4,5]
+L.clear()
+print(L)
