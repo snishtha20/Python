9498d1427c5a184e9f577e44ea6535d8a60e253e
snishtha20 <snishtha2006@gmail.com>
2026-05-08 18:32:33 +0530

    Adding item in List

---
+++ List/addinList.py
@@ -0,0 +1,23 @@
+# append: use to add one element in end
+# L =[1,2,3,4,5]
+# L.append(6)
+# print(L)
+
+# L =[1,2,3,4,5]
+# L.append([6,7,8]) #[1,2,3,4,5,[6,7,8]]
+# print(L)
+
+
+# extend: use to add multiple element in end
+# M = [1,2,3,4,5]
+# M.extend([6,7,8])
+# print(M)
+
+M = [1,2,3,4,5]
+M.extend('Delhi') #[1, 2, 3, 4, 5, 'D', 'e', 'l', 'h', 'i']
+print(M)
+
+# insert 
+# N =[1,2,3,4,5]
+# N.insert(1,100)
+# print(N)
