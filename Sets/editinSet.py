a61b735c386954ed8f5a7bfd1b3147725e7f6043
snishtha20 <snishtha2006@gmail.com>
2026-05-09 18:29:13 +0530

    Accessing & Editing items in Set

---
+++ Sets/editinSet.py
@@ -0,0 +1,10 @@
+# Accessing Items
+s1 = {1,2,3,4}
+print (s1[0:3]) #throw an error
+
+# Editing Items
+s1 = {1,2,3,4}
+s1[0] = 100
+print(s1) #throw an error
+
+# Because sets are unordered so elements don't have fixed index. 
