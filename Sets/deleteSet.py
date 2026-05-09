882d2780c1d657906b9d50128b1d0252c78c2d54
snishtha20 <snishtha2006@gmail.com>
2026-05-09 23:02:07 +0530

    Deleting items in Sets

---
+++ Sets/deleteSet.py
@@ -0,0 +1,26 @@
+# del
+s = {1,2,3,4,5}
+# print(s)
+# del s[0]
+# print(s)
+
+# discard: don not throw error, return same set
+# s.discard(5)
+# print(s)
+# s.discard(50)
+# print(s)
+
+
+# remove: throw error if remove that element which is not present
+# s.remove(5)
+# print(s)
+# s.remove(50)
+# print(s)
+
+
+# pop: remove random element
+# s.pop()
+
+# clear
+s.clear()
+print(s)
