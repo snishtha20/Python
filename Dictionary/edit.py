e241ef38960cba0d93e4546ff50c348694f1bb9e
snishtha20 <snishtha2006@gmail.com>
2026-05-10 13:56:29 +0530

    Editing key-value pairs

---
+++ Dictionary/edit.py
@@ -0,0 +1,12 @@
+s = {
+    'name':'nitish',
+     'college':'bit',
+     'sem':4,
+     'subjects':{
+         'dsa':50,
+         'maths':67,
+         'english':34
+     }
+}
+s['subjects']['dsa'] = 80
+print(s)
