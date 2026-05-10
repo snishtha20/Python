f0e0de2c021fe6878db6bd937e214622de822fdb
snishtha20 <snishtha2006@gmail.com>
2026-05-10 13:55:06 +0530

    Adding key-value pairs

---
+++ Dictionary/add.py
@@ -0,0 +1,31 @@
+# Adding key-value pair
+d4 = dict([('name','nitish'),('age',32),(3,3)])
+
+d4['gender'] = 'male'
+print(d4)
+d4['weight'] = 72
+print(d4)
+
+s = {
+    'name':'nitish',
+     'college':'bit',
+     'sem':4,
+     'subjects':{
+         'dsa':50,
+         'maths':67,
+         'english':34
+     }
+}
+s['subjects']['ds'] = 75
+print(s)
+
+
+# Adding key-value pair
+d4['gender'] = 'male'
+print(d4)
+
+d4['weight'] = 72
+print(d4)
+
+s['subjects']['ds'] = 75
+print(s)
