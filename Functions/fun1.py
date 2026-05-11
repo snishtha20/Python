f1e4a245cc1664f367b7087f6e8ea087f386edbf
snishtha20 <snishtha2006@gmail.com>
2026-05-12 00:06:33 +0530

    Functions

--- Functions/fun1.py
+++ Functions/fun1.py
@@ -19,18 +19,48 @@
 
 # Conclusion: if we use return statement then we have to call the function by declaring/storing a new variable  
 
+# if we did not use return statement in function then the value of return statement will be: None
+# L = [1,2,3]
+# print(L.append(4)) #None because this function does not return anything it only adds element in the end of list.
+# print(L)
 
 # 4th Avg of 3 numbers
-def avg(a,b):
-    print("Average: ", (a+b)/2)
+# def avg(a,b):
+#     print("Average: ", (a+b)/2)
 
-avg(4,6)
+# avg(4,6)
 
 
-# OR
+# # OR
 
-def avg(a,b):
-    return (a+b)/2
+# def avg(a,b):
+#     return (a+b)/2
 
-output = avg(4,6)
-print("Average: ", output)
+# output = avg(4,6)
+# print("Average: ", output)
+
+
+# 5th even/odd
+def is_even(num):
+    # below block of code is called as docstring
+    """ 
+    This function returns if a given number is odd or even
+    input - any valid integer
+    output - odd/even
+    created on - 16th Nov 2022
+    """
+
+    if type(num) == int:
+        if num%2==0:
+            return "Even"
+        else:
+            return "Odd"
+        
+    else:
+        return "Wrong datatype"
+    
+for i in range(1,11):
+    x = is_even(i)
+    print(x)
+
+print(is_even.__doc__) #way to print documentaion(docstring)
